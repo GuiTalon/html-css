bea7c499d1bc81f4e2ca5238229fb3c13b6d7a77
GuiTalon <Guizitto@outlook.com.br>
2023-08-18 15:59:56 -0300

    MENU-LATERAL
    Menu lateral com JS na versão media-query mobile, estou ficando maluco. Mas é muito maneiro

--- exercicios/ex026/script.js
+++ exercicios/ex026/script.js
@@ -1,7 +1,8 @@
-const btnMobile = document.getElementsByClassName('btn-expandir')
+const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu()
-    const nav = document.getElementsByClassName('menu-lateral')
+function toggleMenu() {
+    const nav = document.getElementById('nav');
     nav.classList.toggle('active');
+}
 
-btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('click', toggleMenu);
