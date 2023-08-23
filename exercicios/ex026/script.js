cf5cfdd9ef64c1ed937d71e3d0c031d97bc21bee
GuiTalon <Guizitto@outlook.com.br>
2023-08-23 14:28:59 -0300

    MENU-LATERAL CONCLUÍDO
    Concluído menu-lateral com acessibilidade. Confesso que foi chato e demorado, quebrei a cabeça em muitos momentos, mas o resultado final me agradou, então ta ai. agora provavelmente, vou meter marcha e criar um site encima disso com o grid que eu sei, ou talvez só fazer um "desafio" em outra pasta.

--- exercicios/ex026/script.js
+++ exercicios/ex026/script.js
@@ -1,31 +1,61 @@
 const btnMobile = document.getElementById('btn-mobile');
+const nav = document.getElementById('nav');
+const ItemMenu = document.querySelectorAll('.item-menu');
+let isMenuOpen = false;
 
-function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault()
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active')
-    event.currentTarget.setAttribute('aria-expanded', active);
-    if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+// Função para remover a classe "ativo" de todos os elementos do menu
+function unselectItems() {
+    ItemMenu.forEach((item) => item.classList.remove('ativo'));
+}
+
+// Função para abrir o menu com Enter
+function openMenuWithEnter(event) {
+    if (event.key === 'Enter') {
+        toggleMenu(event);
     }
 }
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
-
-var ItemMenu = document.querySelectorAll('.item-menu')
-
-function selectLink(){
-    ItemMenu.forEach((item)=>
-        item.classList.remove('ativo')
-    )
-    this.classList.add('ativo')
+// Função para ativar ou desativar o menu
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+        isMenuOpen = true;
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+        isMenuOpen = false;
+        unselectItems(); // Chama a função para remover a classe "ativo"
+    }
 }
 
-ItemMenu.forEach((item)=>
-    item.addEventListener('click', selectLink)
-)
+// Adiciona os ouvintes de evento para o botão de menu
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+btnMobile.addEventListener('keydown', openMenuWithEnter);
 
+// Adiciona ouvintes de evento para os itens do menu
+ItemMenu.forEach((item) =>
+    item.addEventListener('click', selectLink)
+);
+
+// Função para marcar ou desmarcar itens do menu
+function selectLink() {
+    const isAlreadySelected = this.classList.contains('ativo');
+
+    unselectItems(); // Remove a classe "ativo" de todos os elementos do menu
+
+    if (!isAlreadySelected) {
+        this.classList.add('ativo');
+    }
+}
+
+// Ouvinte de evento para clicar fora do menu e do botão de menu
+document.addEventListener('click', function (event) {
+    const target = event.target;
+    if (!nav.contains(target) && target !== btnMobile) {
+        unselectItems(); // Remove a classe "ativo" de todos os elementos do menu
+    }
+});
