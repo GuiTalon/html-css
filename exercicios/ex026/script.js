3f5082fa71c937f1f7356b1816bf3731d18e68b6
GuiTalon <Guizitto@outlook.com.br>
2023-08-24 14:50:30 -0300

    menu-lateral xd
    xd

--- exercicios/ex026/script.js
+++ exercicios/ex026/script.js
@@ -3,19 +3,16 @@ const nav = document.getElementById('nav');
 const ItemMenu = document.querySelectorAll('.item-menu');
 let isMenuOpen = false;
 
-// Função para remover a classe "ativo" de todos os elementos do menu
 function unselectItems() {
     ItemMenu.forEach((item) => item.classList.remove('ativo'));
 }
 
-// Função para abrir o menu com Enter
 function openMenuWithEnter(event) {
     if (event.key === 'Enter') {
         toggleMenu(event);
     }
 }
 
-// Função para ativar ou desativar o menu
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
     nav.classList.toggle('active');
@@ -27,35 +24,33 @@ function toggleMenu(event) {
     } else {
         event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
         isMenuOpen = false;
-        unselectItems(); // Chama a função para remover a classe "ativo"
+        unselectItems();
     }
 }
 
-// Adiciona os ouvintes de evento para o botão de menu
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 btnMobile.addEventListener('keydown', openMenuWithEnter);
 
-// Adiciona ouvintes de evento para os itens do menu
+
 ItemMenu.forEach((item) =>
     item.addEventListener('click', selectLink)
 );
 
-// Função para marcar ou desmarcar itens do menu
+
 function selectLink() {
     const isAlreadySelected = this.classList.contains('ativo');
 
-    unselectItems(); // Remove a classe "ativo" de todos os elementos do menu
+    unselectItems();
 
     if (!isAlreadySelected) {
         this.classList.add('ativo');
     }
 }
 
-// Ouvinte de evento para clicar fora do menu e do botão de menu
 document.addEventListener('click', function (event) {
     const target = event.target;
     if (!nav.contains(target) && target !== btnMobile) {
-        unselectItems(); // Remove a classe "ativo" de todos os elementos do menu
+        unselectItems();
     }
 });
